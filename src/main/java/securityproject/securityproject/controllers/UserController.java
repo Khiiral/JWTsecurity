b60269f6852e409f32825c70c8b0b97ac6487186
Federico Curcio <fed.curciorubertini@gmail.com>
2024-01-25 15:51:35 +0100

    Updated user homepage

--- src/main/java/securityproject/securityproject/controllers/UserController.java
+++ src/main/java/securityproject/securityproject/controllers/UserController.java
@@ -2,6 +2,8 @@ package securityproject.securityproject.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,7 +32,6 @@ public class UserController {
         return new ResponseEntity<>(welcomeMessage, HttpStatus.OK);
     }
 
-    
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserData(@PathVariable Long id) throws UserNotFoundException {
