f00b0867306359e670a436d15212d840496e3fb6
Khiiral <virionkhiiral@gmail.com>
2024-02-16 16:19:53 +0100

    added validation on user registration

--- src/main/java/securityproject/securityproject/dto/SignUpRequest.java
+++ src/main/java/securityproject/securityproject/dto/SignUpRequest.java
@@ -1,7 +1,5 @@
 package securityproject.securityproject.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,14 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignUpRequest {
     
-    @NotEmpty(message = "Firstname is required")
+    //@NotEmpty(message = "Firstname is required")
     String firstName;
-    @NotEmpty(message = "Last name is required")
+    //@NotEmpty(message = "Last name is required")
     String lastName;
-    @NotEmpty(message = "Email is required")
-    @Email(message = "Invalid email address")
+    //@NotEmpty(message = "Email is required")
+    //@Email(message = "Invalid email address")
     String email;
-    @NotEmpty(message = "Password is required")
+    //@NotEmpty(message = "Password is required")
     String password;
     
 }
